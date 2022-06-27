6fbf731a4e1bbf9f2bcdbd2dc061d87dd69ee791
Phuong Dai <72750282+deaddesert@users.noreply.github.com>
2022-06-27 22:11:02 +0700

    Update KMeansClustering.py

--- KMeansClustering.py
+++ KMeansClustering.py
@@ -4,72 +4,86 @@ import matplotlib.pyplot as plt
 
 class KMeansClustering:
 
-	def __init__(self, k, tol, max_iter):
-		self.k = k 
-		self.tol = tol
+	def __init__(self, k, max_iter):
+		self.k = k
 		self.max_iter = max_iter
 
 
-	def fit(self, X):
-		self.centroids = dict()
-		self.clusters = dict()
-		cost_function_list = list()
+	def training(self, X):
+		list_of_centroids = list()
+		list_of_clusters = list()
+		cost_function_values = list()
 
-		for i in range(self.max_iter):
+		for n in range(self.max_iter):
 
-			self.centroids[i] = []
-			self.clusters[i] = []
+			M = np.zeros((self.k, X.shape[1])) # centroids
 			cost_function = 0
-			optimization = 0
+			optimized = 0
 
-			for j in range(self.k):
-				self.centroids[i].append(X[j])
+			# Initializing centroids
+			M = X[np.random.choice(X.shape[0], self.k, replace=False)]
 
-			while (optimization == 0):
+			# Assigning Label to Data
+
+			while (optimized == 0):
 				K_clusters = dict()
+				y = np.zeros((X.shape[0], self.k)) # Label vectors
+				X_list = X.tolist()
 				for j in range(self.k):
 					K_clusters[j] = []
 
-				for featureset in X:
-					distances = [np.linalg.norm(featureset - centroid) for centroid in self.centroids[i]]
+				for x in X_list:
+					distances = [np.linalg.norm(x - m) for m in M]
 					cluster = np.argmin(distances)
-					K_clusters[cluster].append(featureset)
+					index = X_list.index(x)
+					K_clusters[cluster].append(x)
+					y[index][cluster] = 1
 
-				prev_centroids = self.centroids[i]
+				# Calculate new centroids
+				prev_centroids = M
 
 				for cluster in K_clusters:
-					self.centroids[i][cluster] = np.mean(K_clusters[cluster], axis=0)
+					M[cluster] = np.mean(K_clusters[cluster], axis=0)
+
+				# Test the convegence
+				comparision = M == prev_centroids
+				if comparision.all():
+					optimized = 1
+
+			list_of_clusters.append(K_clusters)
+			list_of_centroids.append(M)
+
+			for i in range(X.shape[0]):
+				for j in range(self.k):
+					cost_function += (1/len(X))*np.sum((y[i][j]*((X[i] - M[j])**2)))
+
+			cost_function_values.append(cost_function)
+
+		cost_function_min_index = np.argmin(cost_function_values)
+		print(cost_function_min_index)
+
+		# Final result of K-Mean Clustering
+		self.centroids = list_of_centroids[cost_function_min_index]
+		self.clusters = list_of_clusters[cost_function_min_index]
+
+		return [self.centroids, self.clusters, y]
 
 
-				for j in range(len(prev_centroids)):
-					if np.sum((self.centroids[i][j] - prev_centroids[j])/prev_centroids[j]*100.0) <= self.tol:
-						optimization = 1
-
-			for cluster in K_clusters:
-				self.clusters[i].append(K_clusters[cluster])
-
-			for j in range(len(self.clusters[i])):
-				for x in self.clusters[i][j]:
-					cost_function += ((1/len(X))*((x - self.centroids[i][j])**2))
-
-			cost_function_list.append(cost_function)
-
-		self.min = np.argmin(cost_function_list)
-
-		return [self.centroids[self.min], self.clusters[self.min]]
-
-
-	def predict(self, X):
+	def testing(self, X):
 		K_clusters = dict()
+		y = np.zeros((X.shape[0], self.k)) # Label vectors
+		X_list = X.tolist()
 		for j in range(self.k):
 			K_clusters[j] = []
 
-		for featureset in X:
-			distances = [np.linalg.norm(featureset - centroid) for centroid in self.centroids[self.min]]
+		for x in X_list:
+			distances = [np.linalg.norm(x - m) for m in self.centroids]
 			cluster = np.argmin(distances)
-			K_clusters[cluster].append(featureset)
+			index = X_list.index(x)
+			K_clusters[cluster].append(x)
+			y[index][cluster] = 1
 
-		return K_clusters
+		return [K_clusters, y]
 
 
 # Data
@@ -86,7 +100,7 @@ digit = 0
 
 for post_type in post_types:
 	if post_type not in text_digit_vals:
-		text_digit_vals[post_type] = digit 
+		text_digit_vals[post_type] = digit
 		digit += 1
 df['status_type'] = list(map(convert_to_digit, df['status_type']))
 
@@ -98,62 +112,51 @@ np.random.shuffle(train_data)
 
 test_data = dataset[6550:]
 
-model = KMeansClustering(2, 0.0001, 100)
+model = KMeansClustering(2, 60)
 
 # Training model
-training = model.fit(train_data)
-centroids = training[0]
-clusters = training[1]
+centroid_results, train_clusters, train_labels = model.training(train_data)
 
-for centroid in centroids:
+for centroid in centroid_results:
 	print([centroid[1], centroid[2]])
 
 # We will represent clusters and centroids based on num_reactions and num_comments
 
-plt.axis([0, 2500, 0, 1600])
-for features in clusters[0][:200]:
+plt.axis([0, 2500, 0, 3000])
+for features in train_clusters[0][:200]:
 	plt.plot(features[1], features[2], 'ro')
 
-for features in clusters[1][:200]:
+for features in train_clusters[1][:200]:
 	plt.plot(features[1], features[2], 'bo')
 
-for centroid in centroids:
+for centroid in centroid_results:
 	plt.scatter(centroid[1], centroid[2], marker="x",s=150, linewidths=5)
 
 plt.xlabel("Number of Reactions")
 plt.ylabel("Number of Comments")
 
+print(train_labels[:200])
+
 plt.show()
+
+print("\n")
 
 # Testing model
-prediction = model.predict(test_data)
+predicted_clusters, test_labels = model.testing(test_data)
 
-plt.axis([0, 2500, 0, 1600])
-for features in prediction[0]:
+plt.axis([0, 2500, 0, 3000])
+for features in predicted_clusters[0]:
 	plt.plot(features[1], features[2], 'ro')
 
-for features in prediction[1]:
+for features in predicted_clusters[1]:
 	plt.plot(features[1], features[2], 'bo')
 
-for centroid in centroids:
+for centroid in centroid_results:
 	plt.scatter(centroid[1], centroid[2], marker="x",s=150, linewidths=5)
 
 plt.xlabel("Number of Reactions")
 plt.ylabel("Number of Comments")
 
+print(test_labels)
+
 plt.show()
-
-
-
-
-
-
-
-			
-
-
-
-
-
-
-
